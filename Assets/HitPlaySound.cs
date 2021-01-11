d99aaf2a6835cef7b74976baf4d3ad5dfcf6e17d
akuma121 <72017747+akuma121@users.noreply.github.com>
2021-01-11 20:13:15 +0900

    Initial commit

---
+++ Assets/HitPlaySound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public AudioClip sound;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag != "UnityChan2D")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
